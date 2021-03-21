32abcf11511880072369f5b9d46f85ef049bff6d
ng20001 <ng20001@byui.edu>
2021-03-21 14:45:52 +0900

    commit

--- lesson11/js/upcomingEvents.js
+++ lesson11/js/upcomingEvents.js
@@ -22,36 +22,23 @@ fetch(requestURL)
 
     let card = document.createElement('section');
     let word_wrap = document.createElement('div');
-    //let h2 = document.createElement('h2');
+    let linebreak = document.createElement('br');
     let h3 = document.createElement('h3');
     let p = document.createElement('p');
-    //let p2 = document.createElement('p');
-    //let p3 = document.createElement('p');
 
-    //h2.textContent = towns[i].name;
     h3.textContent = 'Upcoming Events';
-    p.textContent = towns[pageNum].events;
 
     var eventArray = String(towns[pageNum].events);
-    
-    for (let i=0; i < eventArray.length; i++){
-      word_wrap.appendChild();
-    }
-
-    //p.textContent = 'Year Founded: ' + towns[i].yearFounded;
-    //p2.textContent = 'Population: ' + towns[i].currentPopulation;
-    //p3.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-
-    //word_wrap.appendChild(h2);
+    eventArray = eventArray.split(',');
     word_wrap.appendChild(h3);
+
+    for (let i=0; i < eventArray.length; i++){
+      p.append(eventArray[i]);
+      p.appendChild(linebreak);
+    }
     word_wrap.appendChild(p);
-    //word_wrap.appendChild(p2);
-    //word_wrap.appendChild(p3);
-    
     card.appendChild(word_wrap);
 
     document.querySelector('#upcoming-event').appendChild(card);
-  //    }
-  //  }  
   });
 
