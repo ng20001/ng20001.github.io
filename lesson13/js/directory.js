975aec78488505fbdeb32adcf4162375b164f2fd
ng20001 <ng20001@byui.edu>
2021-04-08 02:27:52 +0900

    commit

--- lesson13/js/directory.js
+++ lesson13/js/directory.js
@@ -1,5 +1,5 @@
 //const directoryURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-const directoryURL = 'js/directory.json';
+const directoryURL = 'https://raw.githubusercontent.com/ng20001/ng20001.github.io/master/lesson13/js/directory.json';
 
 
 fetch(directoryURL)
@@ -7,45 +7,31 @@ fetch(directoryURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
 
-    const towns = jsonObject['business'];
+    const businesses = jsonObject['business'];
 
-    for (let i = 0; i < towns.length; i++) {
-      if (i == 1 || i == 5 || i == 6) {
+    for (let i = 0; i < businesses.length; i++) {
         let card = document.createElement('section');
         let word_wrap = document.createElement('div');
         let h2 = document.createElement('h2');
-        let h3 = document.createElement('h3');
         let p = document.createElement('p');
         let p2 = document.createElement('p');
-        let p3 = document.createElement('p');
         let image = document.createElement('img');
 
-        h2.textContent = towns[i].name;
-        h3.textContent = towns[i].motto;
-        p.textContent = 'Year Founded: ' + towns[i].yearFounded;
-        p2.textContent = 'Population: ' + towns[i].currentPopulation;
-        p3.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-        image.setAttribute('src', 'images/' + towns[i].photo);
-        image.alt = towns[i].name + i;
+        h2.textContent = 'Name: ' + businesses[i].name;
+        p.textContent = 'Contact: ' + businesses[i].contact;
+        p2.textContent = 'Website: ' + businesses[i].website;
+        image.setAttribute('src', 'images/' + businesses[i].photo);
+        image.alt = businesses[i].name + i;
 
         word_wrap.appendChild(h2);
-        word_wrap.appendChild(h3);
         word_wrap.appendChild(p);
         word_wrap.appendChild(p2);
-        word_wrap.appendChild(p3);
         
-        //card.appendChild(h2);
-        //card.appendChild(h3);
-        //card.appendChild(p);
-        //card.appendChild(p2);
-        //card.appendChild(p3);
         card.appendChild(word_wrap);
         card.appendChild(image);
 
         document.querySelector('div.business-cards').appendChild(card);
-      }
     }  
   });
 
